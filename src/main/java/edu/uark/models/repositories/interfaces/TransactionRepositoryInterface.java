268de23dcfe2e98e6aca78f689416f7b3445d584
rddelcar <rddelcar@email.uark.edu>
2019-04-18 01:06:40 -0500

    Revert "created TransactionQuery and TransactionRepositoryInterface"
    This reverts commit 514cc34cc96b5cd86d6683a79263be8915596d48.

--- src/main/java/edu/uark/models/repositories/interfaces/TransactionRepositoryInterface.java
+++ src/main/java/edu/uark/models/repositories/interfaces/TransactionRepositoryInterface.java
@@ -1,8 +1,5 @@
 package edu.uark.models.repositories.interfaces;
 
-import edu.uark.dataaccess.repository.BaseRepositoryInterface;
-import edu.uark.models.entities.TransactionEntity;
+public class TransactionRepositoryInterface {
 
-public interface TransactionRepositoryInterface extends BaseRepositoryInterface<TransactionEntity> {
-//			TransactionEntity byLookupCode(String lookupCode);
-		}
+}
