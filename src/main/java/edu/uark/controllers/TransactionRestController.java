69c4c77e7851f3dc97cb423a4731653f038424fe
rddelcar <rddelcar@email.uark.edu>
2019-04-18 01:51:28 -0500

    some changes on transactionCreateCommand.java and transactionRestController.java
    still has a lot of bugs

--- src/main/java/edu/uark/controllers/TransactionRestController.java
+++ src/main/java/edu/uark/controllers/TransactionRestController.java
@@ -1,25 +1,48 @@
-//package edu.uark.controllers;
-//
-//import edu.uark.commands.products.*;
-//import edu.uark.models.api.Product;
-//import edu.uark.models.api.Transaction;
-//import org.springframework.web.bind.annotation.*;
-//
-//import java.util.List;
-//import java.util.UUID;
-//
-//@RestController
-//@RequestMapping(value = "/api/transaction")
-//public class TransactionRestController {
-//
-//	@RequestMapping(value = "/", method = RequestMethod.GET)
-//	public List<Transaction> getProducts() {
-//		return (new TransactionsQuery()).execute();
-//	}
-//
-//	@ResponseBody
-//	@RequestMapping(value = "/test", method = RequestMethod.GET)
-//	public String test() {
-//		return "Successful test. (TransactionRestController)";
-//	}
-//}
+package edu.uark.controllers;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import edu.uark.commands.products.ProductCreateCommand;
+import edu.uark.models.api.Product;
+import edu.uark.commands.transaction.TransactionCreateCommand;
+//import edu.uark.commands.transaction.ProductDeleteCommand;
+import edu.uark.commands.transaction.TransactionQuery;
+//import edu.uark.commands.transaction.ProductUpdateCommand;
+import edu.uark.commands.transaction.TransactionQuery;
+import edu.uark.models.api.Product;
+
+
+
+
+import edu.uark.commands.products.*;
+import edu.uark.models.api.Product;
+import edu.uark.models.api.Transaction;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping(value = "/api/transaction")
+public class TransactionRestController {
+
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public List<Transaction> getProducts() {
+		return (new TransactionQuery()).execute();
+	}
+
+	@ResponseBody
+	@RequestMapping(value = "/test", method = RequestMethod.GET)
+	public String test() {
+		return "Successful test. (TransactionRestController)";
+	}
+	
+	@RequestMapping(value = "/", method = RequestMethod.POST)
+	public Transaction createTransaction(@RequestBody Product product) {
+		return (new TransactionCreateCommand()).
+			setApiTransaction(product).
+			execute();
+	}
+	
+}
