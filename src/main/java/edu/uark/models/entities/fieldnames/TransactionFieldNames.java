f1031750bb3e805af45625b63c8d7263b6ac4ef7
raffiyell <46834718+raffiyell@users.noreply.github.com>
2019-04-22 19:53:37 -0500

    GET Request for Transaction is now working

--- src/main/java/edu/uark/models/entities/fieldnames/TransactionFieldNames.java
+++ src/main/java/edu/uark/models/entities/fieldnames/TransactionFieldNames.java
@@ -3,7 +3,7 @@ package edu.uark.models.entities.fieldnames;
 import edu.uark.dataaccess.entities.BaseFieldNames;
 
 public class TransactionFieldNames extends BaseFieldNames {
-	public static final String RECORD_ID = "recordid";
+	//public static final String RECORD_ID = "recordid";
 	public static final String CASHIER_ID = "cashierid";
 	public static final String TOTAL_AMOUNT = "totalamount";
 	public static final String TRANSACTION_TYPE = "transactiontype";
