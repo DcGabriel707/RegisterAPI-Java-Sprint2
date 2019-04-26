531f567d524f71055919d53703600aebf62b4b71
raffiyell <46834718+raffiyell@users.noreply.github.com>
2019-04-26 01:22:27 -0500

    Update TransactionEntryFieldNames.java

--- src/main/java/edu/uark/models/entities/fieldnames/TransactionEntryFieldNames.java
+++ src/main/java/edu/uark/models/entities/fieldnames/TransactionEntryFieldNames.java
@@ -5,7 +5,7 @@ import edu.uark.dataaccess.entities.BaseFieldNames;
 public class TransactionEntryFieldNames extends BaseFieldNames {
 
 	public static final String QUANTITY = "quantity";
-	public static final String ID = "id";
+//	public static final String ID = "id";
 	public static final String LOOKUPCODE = "lookupcode";
 	public static final String REFERENCE_ID = "referenceid";
 	public static final String PRICE = "price";
